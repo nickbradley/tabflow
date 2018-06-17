229d6d9aab74027046efb987fbaeadcc3752f3cf
Nick Bradley <nickbradley@live.ca>
2018-06-17 11:54:19 +0200

    closes #2

--- background.js
+++ background.js
@@ -1,94 +1,78 @@
-/**
- * Gets the "events" object from the local store asynchronously. If the object doesn't exist, initializes it. 
- */
-async function getStoredEvents() {
-    return new Promise((resolve, reject) => {
-        chrome.storage.local.get({
-            activated: [],
-            attached: [],
-            created: [],
-            detached: [],
-            moved: [],
-            removed: [],
-            updated: []
-        }, (items) => {
-            if (chrome.runtime.lastError) {
-                reject(chrome.runtime.lastError);
-            }
-            resolve(items);
-        });
-    });
+function handleActivated(activeInfo) {
+    let record = {
+        [Date.now()]: {
+            tabId: activeInfo.tabId,
+            tabEvent: "activated"
+        }
+    };
+    chrome.storage.local.set(record);
 }
 
-async function handleActivated(activeInfo) {
-    // Will only block the first time the events object is retrieved from storage
-    events = await getEvents;
-    events.activated.push({
-        tabId: activeInfo.tabId,
-        timestamp: Date.now()
-    });
-    chrome.storage.local.set({ "activated": events.activated });
+function handleAttached(tabId, attachInfo) {
+    let record = {
+        [Date.now()]: {
+            tabId,
+            attachInfo,
+            tabEvent: "attached"
+        }
+    };
+    chrome.storage.local.set(record);
 }
 
-async function handleAttached(tabId, attachInfo) {
-    events = await getEvents;
-    events.attached.push({
-        tabId,
-        attachInfo,
-        timestamp: Date.now()
-    });
-    chrome.storage.local.set({ "attached": events.attached });
+function handleCreated(tab) {
+    let record = {
+        [Date.now()]: {
+            tab,
+            tabEvent: "created"
+        }
+    };
+    chrome.storage.local.set(record);
 }
 
-async function handleCreated(tab) {
-    events = await getEvents;
-    events.created.push({
-        tab,
-        timestamp: Date.now()
-    });
-    chrome.storage.local.set({ "created": events.created });
+function handleDetached(tabId, detachInfo) {
+    let record = {
+        [Date.now()]: {
+            tabId,
+            detachInfo,
+            tabEvent: "detached"
+        }
+    };
+    chrome.storage.local.set(record);
 }
 
-async function handleDetached(tabId, detachInfo) {
-    events = await getEvents;
-    events.detached.push({
-        tabId,
-        detachInfo,
-        timestamp: Date.now()
-    });
-    chrome.storage.local.set({ "detached": events.detached });
+function handleMoved(tabId, moveInfo) {
+    let record = {
+        [Date.now()]: {
+            tabId,
+            moveInfo,
+            tabEvent: "moved"
+        }
+    };
+    chrome.storage.local.set(record);
 }
 
-async function handleMoved(tabId, moveInfo) {
-    events = await getEvents;
-    events.moved.push({
-        tabId,
-        moveInfo,
-        timestamp: Date.now()
-    });
-    chrome.storage.local.set({ "moved": events.moved });
+function handleRemoved(tabId, removeInfo) {
+    let record = {
+        [Date.now()]: {
+            tabId,
+            removeInfo,
+            tabEvent: "removed"
+        }
+    };
+    chrome.storage.local.set(record);
 }
 
-async function handleRemoved(tabId, removeInfo) {
-    events = await getEvents;
-    events.removed.push({
-        tabId,
-        removeInfo,
-        timestamp: Date.now()
-    });
-    chrome.storage.local.set({ "removed": events.removed });
-}
-
-async function handleUpdated(tabId, changeInfo, tab) {
-    events = await getEvents;
+function handleUpdated(tabId, changeInfo, tab) {
     // Only record changes to the URL (e.g. when a user navigates to a new page) and status (i.e. loading vs complete)
     if (changeInfo.url || changeInfo.status) {
-        events.updated.push({
-            tabId,
-            changeInfo,
-            timestamp: Date.now()
-        });
-        chrome.storage.local.set({ "updated": events.updated });
+        let record = {
+            [Date.now()]: {
+                tabId,
+                changeInfo,
+                tabEvent: "updated"
+            }
+        };
+        chrome.storage.local.set(record);
     }
 }
 
@@ -112,13 +96,4 @@ function stop() {
     chrome.tabs.onUpdated.removeListener(handleUpdated);
 }
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request === "getEvents") {
-        getStoredEvents().then(sendResponse);
-        // tell chrome this is async
-        return true;
-    }
-});
-
-let getEvents = getStoredEvents();
 start();
--- popup.js
+++ popup.js
@@ -2,17 +2,18 @@
  * Pull all the events from storage and download in JSON. 
  */
 function btnExportClick() {
-    chrome.runtime.sendMessage("getEvents", (events) => {
+    chrome.storage.local.get(null, (items) => {
         if (chrome.runtime.lastError) {
             console.error(chrome.runtime.lastError);
             return;
         }
-        let json = JSON.stringify(events);
+        let json = JSON.stringify(items);
         let blob = new Blob([ json ], { type: "application/json" });
         chrome.downloads.download({
             url: URL.createObjectURL(blob),
             filename: "tabflow.json"
         });
     });
+
 }
 document.getElementById("btnExport").addEventListener("click", btnExportClick);
